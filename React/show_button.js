24f71239ab16646754b188ab7d6a62f878787a26
fernal73 <linus.fernandes@gmail.com>
2021-06-02 16:49:31 +0530

    add eslint rules and first files

---
+++ React/show_button.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const e = React.createElement;
+
+class ShowButton extends React.Component {
+
+  render() {
+
+    return e(
+      "button",
+      { onClick: () => alert("Hello there!") },
+      "Show"
+    );
+  }
+}
+
+const domContainer = document.querySelector("#show_button_container");
+ReactDOM.render(e(ShowButton), domContainer);
