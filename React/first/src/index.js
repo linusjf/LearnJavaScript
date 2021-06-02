f8ab1011f43a1f02497d527b266a47d466aeb223
fernal73 <linus.fernandes@gmail.com>
2021-06-03 02:58:37 +0530

    Create first react app

---
+++ React/first/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Test extends React.Component {
+
+  render() {
+    return <p>Hello there!</p>;
+  }
+}
+ReactDOM.render(<Test />, document.getElementById('root'));
