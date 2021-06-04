d63346a56de75e44b2619877f51b62b258f4aad2
fernal73 <linus.fernandes@gmail.com>
2021-06-04 15:37:29 +0530

    Add webpackdemo3

---
+++ React/webpackdemo3/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  resolve: {
+    modules: ["/data/data/com.termux/files/usr/lib/node_modules"]
+  },
+  resolveLoader: {
+    modules: ["/data/data/com.termux/files/usr/lib/node_modules"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ]
+};
