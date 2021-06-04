b4c8b95c4779e402d31ee40ac2061ae0a7c803a7
fernal73 <linus.fernandes@gmail.com>
2021-06-05 04:49:56 +0530

    Add webpackdemo6:

---
+++ React/webpackdemo6/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry:{ 
+    index: "./src/index.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "/",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  resolve: {
+    modules: ["/data/data/com.termux/files/usr/lib/node_modules"]
+  },
+  resolveLoader: {
+    modules: ["/data/data/com.termux/files/usr/lib/node_modules"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
+};
