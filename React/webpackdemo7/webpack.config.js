92176b85865af604b077ccbc6572577813056b3d
fernal73 <linus.fernandes@gmail.com>
2021-06-05 11:44:00 +0530

    Add webpack.config.js

--- React/webpackdemo7/webpack.config.js
+++ React/webpackdemo7/webpack.config.js
@@ -1,28 +1,31 @@
 const path = require("path");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "webpack-numbers.js",
-    library: {
-      name: "webpackNumbers",
-      type: "umd",
+module.exports = env => {
+  console.log(env);
+  return {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "webpack-numbers.js",
+      library: {
+        name: "webpackNumbers",
+        type: "umd",
+      },
     },
-  },
-  resolve: {
-    modules: ["/data/data/com.termux/files/usr/lib/node_modules"]
-  },
-  resolveLoader: {
-    modules: ["/data/data/com.termux/files/usr/lib/node_modules"]
-  },
-  externals: {
-    lodash: {
-      commonjs: "lodash",
-      commonjs2: "lodash",
-      amd: "lodash",
-      root: "_",
+    resolve: {
+      modules: ["/data/data/com.termux/files/usr/lib/node_modules"]
     },
-  },
+    resolveLoader: {
+      modules: ["/data/data/com.termux/files/usr/lib/node_modules"]
+    },
+    externals: {
+      lodash: {
+        commonjs: "lodash",
+        commonjs2: "lodash",
+        amd: "lodash",
+        root: "_",
+      },
+    },
+  };
 };
