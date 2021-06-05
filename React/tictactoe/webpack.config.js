75df780d3e2deaafd398a60a6350e96ddb9c3785
fernal73 <linus.fernandes@gmail.com>
2021-06-05 19:14:19 +0530

    Convert to webpack

---
+++ React/tictactoe/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true
+  },
+  resolve: {
+    modules: ["/data/data/com.termux/files/usr/lib/node_modules"]
+  },
+  resolveLoader: {
+    modules: ["/data/data/com.termux/files/usr/lib/node_modules"]
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-object-rest-spread"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "TicTacToe",
+      template: "index.html"
+    })
+  ]
+};
