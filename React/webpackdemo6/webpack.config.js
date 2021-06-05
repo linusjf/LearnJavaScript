26bec5e2439efffbc9380378bcd0ec89451596d2
fernal73 <linus.fernandes@gmail.com>
2021-06-05 10:33:00 +0530

    Add print

--- React/webpackdemo6/webpack.config.js
+++ React/webpackdemo6/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     index: "./src/index.js",
   },
   output: {
-    filename: "[name].bundle.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
     publicPath: "/",
@@ -24,7 +24,20 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Development",
+      title: "Caching",
     }),
   ],
+  optimization: {
+    moduleIds: "deterministic",
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
 };
