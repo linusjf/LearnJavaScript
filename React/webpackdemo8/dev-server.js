58289464fafd5b1b338dd976e56d3f8f9aea1be4
fernal73 <linus.fernandes@gmail.com>
2021-06-05 15:12:55 +0530

    Remove options

--- React/webpackdemo8/dev-server.js
+++ React/webpackdemo8/dev-server.js
@@ -2,15 +2,15 @@ const webpackDevServer = require("webpack-dev-server");
 const webpack = require("webpack");
 
 const config = require("./webpack.config.js");
-const options = {
+/**const options = {
   contentBase: "./dist",
   hot: true,
   host: "localhost",
-};
+};*/
 
-webpackDevServer.addDevServerEntrypoints(config, options);
+//webpackDevServer.addDevServerEntrypoints(config, options);
 const compiler = webpack(config);
-const server = new webpackDevServer(compiler, options);
+const server = new webpackDevServer(compiler);
 
 server.listen(5000, "localhost", () => {
   console.log("dev server listening on port 5000");
