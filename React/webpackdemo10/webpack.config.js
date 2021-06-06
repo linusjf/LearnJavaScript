720d37a8f3bf588911e8a8c3fe322842b25c380f
fernal73 <linus.fernandes@gmail.com>
2021-06-06 08:19:44 +0530

    Lazy loading

---
+++ React/webpackdemo10/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry:{ 
+    index: "./src/index.js",
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "/",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  resolve: {
+    modules: ["/data/data/com.termux/files/usr/lib/node_modules"]
+  },
+  resolveLoader: {
+    modules: ["/data/data/com.termux/files/usr/lib/node_modules"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Caching",
+    }),
+  ],
+  optimization: {
+    moduleIds: "deterministic",
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
+};
