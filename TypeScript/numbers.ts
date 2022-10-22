3177781f298641294be6b9befeb6aba1e2efaba5
fernal73 <linus.fernandes@gmail.com>
2022-10-22 13:39:35 +0530

    feat: update numbers example

--- TypeScript/numbers.ts
+++ TypeScript/numbers.ts
@@ -2,3 +2,14 @@ console.log("Minus infinity: " + Number.NEGATIVE_INFINITY);
 console.log("Positive infinity: " + Number.POSITIVE_INFINITY);
 console.log("Maximum: " + Number.MAX_VALUE); 
 console.log("Minimum: " + Number.MIN_VALUE);
+var num = new Number(100.1234); 
+// Output a number that has three decimals 
+console.log(num.toFixed(3)); 
+// Output a number with length 4 
+console.log(num.toPrecision(4)); 
+// Output a decimal number 
+console.log(num.toString()); 
+// Output a binary number
+console.log(num.toString(2));
+// Output a hexadecimal number
+console.log(num.toString(16));
