dfd2926f42bc1120c1f86f5f4736e05c80ede4c5
fernal73 <linus.fernandes@gmail.com>
2022-10-22 15:38:38 +0530

    feat: add arithmetic example

---
+++ TypeScript/arithmetic.ts
@@ -0,0 +1,11 @@
+var x:number = 20 
+var y:number = 2 
+var result:number = 0
+result = x + y; 
+console.log("20 + 2 = " + result);  
+result = x / y; 
+console.log("20 / 2 = " + result); 
+result = x % y; 
+console.log("20 % 2 = " + result); 
+result = ++x; 
+console.log("++x = " + result); 
