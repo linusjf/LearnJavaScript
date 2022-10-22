4b6145621c0f1a129ae16f2da2aab7339845d8db
fernal73 <linus.fernandes@gmail.com>
2022-10-22 13:34:58 +0530

    feat: add numbers example

---
+++ TypeScript/numbers.ts
@@ -0,0 +1,4 @@
+console.log("Minus infinity: " + Number.NEGATIVE_INFINITY);
+console.log("Positive infinity: " + Number.POSITIVE_INFINITY);
+console.log("Maximum: " + Number.MAX_VALUE); 
+console.log("Minimum: " + Number.MIN_VALUE);
