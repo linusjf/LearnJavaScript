703066f9b449054b8690b468a86454c38aa58dae
fernal73 <linus.fernandes@gmail.com>
2022-10-22 17:44:26 +0530

    feat: add assign example

---
+++ TypeScript/assign.ts
@@ -0,0 +1,15 @@
+var x:number = 20; 
+var y:number = 2;  
+x += y; 
+// x=x+y returns 22 
+console.log(x); 
+var x:number = 20; 
+var y:number = 2;  
+x /= y; 
+// x=x/y  returns 10 
+console.log(x);
+var x:number = 20; 
+var y:number = 2;  
+x %= y; 
+// x=x%y returns
+console.log(x);
