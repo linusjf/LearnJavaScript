937f121976cb48befb3aaaba11ed2642ef0d3625
fernal73 <linus.fernandes@gmail.com>
2022-10-22 10:00:04 +0530

    feat: add variables example

---
+++ TypeScript/variables.ts
@@ -0,0 +1,6 @@
+var myBook:string = "Go in 8 Hours"; 
+var myScore:number = 100; 
+var myStory:boolean = true;
+console.log("My book is: " + myBook) 
+console.log("My exam score is: " + myScore) 
+console.log("My story is: " + myStory)
