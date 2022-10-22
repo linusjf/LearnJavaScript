b6bcbc394379bfebcf849024e2ae0d3a298a6564
fernal73 <linus.fernandes@gmail.com>
2022-10-22 10:34:34 +0530

    feat: add any example

---
+++ TypeScript/any.ts
@@ -0,0 +1,11 @@
+var myVariable: any; 
+// myVariable is “any” type 
+myVariable = 100; 
+// myVariable is “number” type 
+console.log(myVariable); 
+myVariable = 'Scala in 8 Hours';  
+// myVariable is “string” type 
+console.log(myVariable); 
+myVariable = false; 
+// myVariable is “Boolean” type 
+console.log(myVariable);
