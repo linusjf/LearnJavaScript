d1f3a28a16ea1d39f937e8f5b247fe4fb556f61a
fernal73 <linus.fernandes@gmail.com>
2022-10-24 16:57:31 +0530

    feat: add intersection example

---
+++ TypeScript/intersection.ts
@@ -0,0 +1,28 @@
+interface Knife {
+  cut():void;
+} 
+interface BottleOpener { 
+  openBottle():void; 
+}
+interface Screwdriver { 
+  turnScrew():void;
+}
+type SwissArmyKnife = Knife & BottleOpener & Screwdriver; 
+function use(tool: SwissArmyKnife) { 
+  console.log("I can do anything!"); 
+  tool.cut(); 
+  tool.openBottle(); 
+  tool.turnScrew(); 
+}
+const sak:SwissArmyKnife = {
+ cut():void  {
+   console.log("Cut.");
+ }, 
+ openBottle():void  {
+   console.log("Opened bottle.");
+ },
+ turnScrew():void  {
+   console.log("Turned screw.");
+ } 
+}
+use(sak);
