8ab5d04db91abf016e1e687d3ef8fb4d07bd3441
fernal73 <linus.fernandes@gmail.com>
2022-10-25 07:35:54 +0530

    feat: start enum example

---
+++ TypeScript/enums.ts
@@ -0,0 +1,7 @@
+const enum NinjaActivity { 
+  Espionage , 
+  Sabotage, 
+  Assassination } 
+let myFavoriteNinjaActivity = NinjaActivity.Espionage; 
+console.log(myFavoriteNinjaActivity); 
+console.log(NinjaActivity["Sabotage"]); 
