e4a3d551182f46654f1244b2aa390971c7f8fefd
fernal73 <linus.fernandes@gmail.com>
2022-10-29 14:47:33 +0530

    feat: add guards examples

---
+++ TypeScript/runfunc.ts
@@ -0,0 +1,21 @@
+interface IRushable { 
+  run(): void; 
+}
+interface IRusher { 
+  runSafe<T extends IRushable>(runnable: T): void; 
+}
+class Rusher implements IRusher {
+  public runSafe<T extends IRushable>(runnable: T): void { 
+    try { 
+      runnable.run(); 
+    } catch(e) { 
+    } 
+  } 
+}
+
+function runSafe<T extends IRushable>(runnable: T): void { 
+  try { 
+    runnable.run(); 
+  } catch(e) { 
+  } 
+}
