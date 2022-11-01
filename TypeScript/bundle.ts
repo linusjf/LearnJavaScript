f7b15245e423e1c135d44b278ce5450f06f9dd8a
fernal73 <linus.fernandes@gmail.com>
2022-11-01 21:00:56 +0530

    feat: add mixins example

---
+++ TypeScript/bundle.ts
@@ -0,0 +1,4 @@
+import * as Bundle from "./adams";
+Bundle.hello(Bundle.answerToLifeTheUniverseAndAnything);
+console.log(Bundle.unused);
+
---
+++ TypeScript/mixins.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env ts-node
+class Flies {
+  fly() {
+    console.log("Is it a bird? Is it a plane?");
+  }
+}
+class Climbs {
+climb() {
+console.log("My spider sense is tingling.");
+}
+}
+
+class Bulletproof {
+deflect() {
+console.log("My wings are a shield of steel.");
+}
+}
+
+class BeetleGuy implements Climbs, Bulletproof {
+  climb!:() => void;
+  deflect!:() => void;
+}
+
+applyMixins(BeetleGuy,[Climbs,Bulletproof]);
+
+function applyMixins(derivedCtor:any,baseCtors:any[]) {
+baseCtors.forEach(baseCtor => {
+Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
+  if (name!=='constructor') {
+derivedCtor.prototype[name] = baseCtor.prototype[name];}
+});
+});
+  }
