e476e1e52e108e060514ba0a422a085089f59862
Linus Fernandes <linus.fernandes@gmail.com>
2024-03-29 22:04:16 +0530

    feat: add essential javascript examples

--- Essentials/destructuring.js
+++ Essentials/destructuring.js
@@ -164,3 +164,70 @@ const key = "z";
   // to evaluate the default value.
   console.log(b);
 }
+{
+  const { a, ...others } = { a: 1, b: 2, c: 3 };
+  console.log(others); // { b: 2, c: 3 }
+
+  const [first, ...others2] = [1, 2, 3];
+  console.log(others2); // [2, 3]
+}
+{
+  const foo = ["one", "two", "three"];
+  const [red, yellow, green] = foo;
+  console.log(red); // "one"
+  console.log(yellow); // "two"
+  console.log(green); // "three"
+}
+{
+  const foo = ["one", "two"];
+
+  const [red, yellow, green, blue] = foo;
+  console.log(red); // "one"
+  console.log(yellow); // "two"
+  console.log(green); // undefined
+  console.log(blue); // undefined
+}
+{
+  let a = 1;
+  let b = 3;
+
+  [a, b] = [b, a];
+  console.log(a); // 3
+  console.log(b); // 1
+
+  const arr = [1, 2, 3];
+  [arr[2], arr[1]] = [arr[1], arr[2]];
+  console.log(arr); // [1, 3, 2]
+}
+{
+  function f() {
+    return [1, 2];
+  }
+
+  const [a, b] = f();
+  console.log(a); // 1
+  console.log(b); // 2
+}
+{
+  function f() {
+    return [1, 2, 3];
+  }
+
+  const [a, , b] = f();
+  console.log(a); // 1
+  console.log(b); // 3
+
+  const [c] = f();
+  console.log(c); // 1
+}
+{
+  [, ,] = f();
+}
+{
+  const [a, b, ...{ length }] = [1, 2, 3, 4];
+  console.log(a, b, length); // 1 2 2
+}
+{
+  const [a, b, ...[c, d]] = [1, 2, 3, 4];
+  console.log(a, b, c, d); // 1 2 3 4
+}
