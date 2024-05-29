bd8226c58886e93aa5748811cfd99df65a209227
Linus Fernandes <linus.fernandes@gmail.com>
2024-05-29 10:26:12 +0530

    feat: update lint script and fix violations

--- React/hello/src/Hello.jsx
+++ React/hello/src/Hello.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from "react";
 
 class HelloWorld extends React.Component {
   render() {
-    return < h1 > Hello, World! < /h1>
+    return <h1>Hello, World!</h1>;
   }
 }
 
@@ -12,17 +12,12 @@ class Hello extends React.Component {
     let firstName = this.props.name.split(" ")[0];
     this.state = {
       name: firstName
-    }
+    };
   }
 
   render() {
-    return < h1 > Hello, {
-      this.state.name
-    }! < /h1>
+    return <h1> Hello, {this.state.name}! </h1>;
   }
 }
 
-export {
-  HelloWorld,
-  Hello
-}
+export { HelloWorld, Hello };
